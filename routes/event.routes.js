be36f96764c110ca69349a6e4fd0b1602ff89cea
shivambirle7-ship-it <231662069+shivambirle7-ship-it@users.noreply.github.com>
2026-04-25 16:58:54 +0530

    Update event.routes.js

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -5,12 +5,12 @@ const validate = require('../middleware/validator.middleware');
 const authorize = require('../middleware/user.middleware');
 const { createEventSchema, updateEventSchema, bookEventSchema } = require('../validators/event.validator');
 
-router.get('/events', authorize(['admin', 'user']), eventController.getAllEvents);
-router.get('/events/:id', authorize(['admin', 'user']), eventController.getEventById);
-router.post('/events', authorize(['admin']), validate(createEventSchema), eventController.createEvent);
-router.patch('/events/:id', authorize(['admin']), validate(updateEventSchema), eventController.updateEvent);
-router.delete('/events/:id', authorize(['admin']), eventController.deleteEvent);
+router.get('/', authorize(['admin', 'user']), eventController.getAllEvents);
+router.get('/:id', authorize(['admin', 'user']), eventController.getEventById);
+router.post('/', authorize(['admin']), validate(createEventSchema), eventController.createEvent);
+router.patch('/:id', authorize(['admin']), validate(updateEventSchema), eventController.updateEvent);
+router.delete('/:id', authorize(['admin']), eventController.deleteEvent);
 
-router.post('/events/:id/book', authorize(['user']), validate(bookEventSchema), eventController.bookEvent);
+router.post('/:id/book', authorize(['user']), validate(bookEventSchema), eventController.bookEvent);
 
 module.exports = router;
